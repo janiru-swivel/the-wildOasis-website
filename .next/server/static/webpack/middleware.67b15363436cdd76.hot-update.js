78b01a5d15d4b58ea3d10b2ce7b5b5765bcb83da
janiru-swivel <janiru@swivelgroup.com.au>
2024-10-01 12:00:07 +0530

    initial commit

---
+++ .next/server/static/webpack/middleware.67b15363436cdd76.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("middleware",{
+
+/***/ "(middleware)/./app/_lib/supabase.js":
+/*!******************************!*\
+  !*** ./app/_lib/supabase.js ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _supabase_supabase_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @supabase/supabase-js */ \"(middleware)/./node_modules/@supabase/supabase-js/dist/module/index.js\");\n\nconst supabaseUrl = \"https://qpdaokefcxkjoptafoat.supabase.co\";\nconst supabaseKey = process.env.SUPABASE_KEY;\nconst supabase = (0,_supabase_supabase_js__WEBPACK_IMPORTED_MODULE_0__.createClient)(supabaseUrl, supabaseKey);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKG1pZGRsZXdhcmUpLy4vYXBwL19saWIvc3VwYWJhc2UuanMiLCJtYXBwaW5ncyI6Ijs7QUFBcUQ7QUFDckQsTUFBTUMsY0FBYztBQUNwQixNQUFNQyxjQUFjQyxRQUFRQyxHQUFHLENBQUNDLFlBQVk7QUFDNUMsTUFBTUMsV0FBV04sbUVBQVlBLENBQUNDLGFBQWFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8uL2FwcC9fbGliL3N1cGFiYXNlLmpzP2VmM2MiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgY3JlYXRlQ2xpZW50IH0gZnJvbSBcIkBzdXBhYmFzZS9zdXBhYmFzZS1qc1wiO1xuY29uc3Qgc3VwYWJhc2VVcmwgPSBcImh0dHBzOi8vcXBkYW9rZWZjeGtqb3B0YWZvYXQuc3VwYWJhc2UuY29cIjtcbmNvbnN0IHN1cGFiYXNlS2V5ID0gcHJvY2Vzcy5lbnYuU1VQQUJBU0VfS0VZO1xuY29uc3Qgc3VwYWJhc2UgPSBjcmVhdGVDbGllbnQoc3VwYWJhc2VVcmwsIHN1cGFiYXNlS2V5KTtcbiJdLCJuYW1lcyI6WyJjcmVhdGVDbGllbnQiLCJzdXBhYmFzZVVybCIsInN1cGFiYXNlS2V5IiwicHJvY2VzcyIsImVudiIsIlNVUEFCQVNFX0tFWSIsInN1cGFiYXNlIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(middleware)/./app/_lib/supabase.js\n");
+
+/***/ })
+
+});
